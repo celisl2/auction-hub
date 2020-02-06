157bd5a8ea5f79b6adbbe2a4ccea9516a586ec93
cashtonCode <44584924+cashtonCode@users.noreply.github.com>
2020-02-06 14:53:56 -0500

    Create get_event_location.js
    This is a first step, we can use this as a basis going forward.

---
+++ pages/api/get_event_location.js
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
+// Author : Cashton Christensen
+// Date Last Modified : 2/6/2020
+// Description :
+//      Summary : Return the location text from the database to the front end
+//      Inputs  : Text id of the specific auction_event
+//      Outputs : Text value from the auction_event location field
+////////////////////////////////////////////////////////////////////////////////
+function get_event_location (eventId) {
+    // query the database using eventId and return just the location text value
+    // location = //query//
+    // return location;
+}
