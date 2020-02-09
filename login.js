2058160c4bbcebcdc622c5aba5508b6a8cf20095
jestes0 <39225989+jestes0@users.noreply.github.com>
2020-02-09 09:00:14 -0500

    Update login.js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ const LogInForm = () => {
         <html>
         <head>
         <script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
-        <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
+        <Link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
         </head>
         </html>
     );
