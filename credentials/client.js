6ddb980ad759767b345c53e059e21a02ed97dc4a
jestes0 <39225989+jestes0@users.noreply.github.com>
2020-02-09 11:09:26 -0500

    Update client.js

--- credentials/client.js
+++ credentials/client.js
@@ -1,10 +1,11 @@
 module.exports = {
-  // paste your firebase config here:
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+const firebaseConfig = {
+  apiKey: "AIzaSyCxOo7KE7_SctNyRIRpvcDyVua-kIRH77Y",
+  authDomain: "fir-testing-bd63e.firebaseapp.com",
+  databaseURL: "https://fir-testing-bd63e.firebaseio.com",
+  projectId: "fir-testing-bd63e",
+  storageBucket: "fir-testing-bd63e.appspot.com",
+  messagingSenderId: "1087747058962",
+  appId: "1:1087747058962:web:de31723c90bbef1f3d17cb"
+};
 }
