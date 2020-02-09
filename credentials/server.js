82956ba84104746d196d2af6ed3283c15cceddfa
jestes0 <39225989+jestes0@users.noreply.github.com>
2020-02-09 11:04:46 -0500

    Update server.js

--- credentials/server.js
+++ credentials/server.js
@@ -1,13 +1,12 @@
 module.exports = {
-  // paste your firebase config here:
-  type: '',
-  project_id: '',
-  private_key_id: '',
-  private_key: '',
-  client_email: '',
-  client_id: '',
-  auth_uri: '',
-  token_uri: '',
-  auth_provider_x509_cert_url: '',
-  client_x509_cert_url: '',
+  "type": "service_account",
+  "project_id": "fir-testing-bd63e",
+  "private_key_id": "cc7f937908430cc0e108dfd1540b048ff051d2bb",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDM/q963o7GKcJc\nCxiW2u6XQx7VMGMFYoWx5cfsRayVM2Vo8scMHtcc0chVGHL+ODoMkZ03I0aiDWtl\naukTxt8szV+Ssv+F8RPlE8QiSvaS8J5btGpKkt0ulVsdNCgLQgD7GX6B69If/+uY\nzH7Qs5zGOF77QhmQB9mUSwidBxmvREvR5iUHsuLX2BAerQeyFbYhYEnTmocCkzO8\n18PgWxHSFKPR+5y2IM84C9i/TtwEmvDAg5U+Jbrp6vM4zt2LCxKKF1CD8Fz5ndVy\n5EuK4OZCn0MOyJyqzHGVVctxKSpjyjHKX0LGHs/j5F96SXfYgXpTsnq0J42nmeEC\nnHEl+6u7AgMBAAECggEAGtRsoC63zgzSINzc64Wc+5Qv2WQ7WY2TBP0XhP97fo2b\nBLjdv2Lb8awtRaHtDwPJ6TI3uus1h+ksuLnc5pc9R25bx2YBFoU+ftlu7rQUAJhY\niZUb4kXFiB7JLZHReeO6ICMukQ+N0V1b9wQwxtzzNGhVWhpFITo9ynBloLO4s10H\nQDidQ5YfwCiPlOxn/xDdMoL2w3UDrfyDrBVzswXbAguO99hvOa/+gDfbE4+oaq4a\n/BtJShOY5sxJ+x9bhV28PFtXl9EiWyL95bTb9dvaIEqZKPfXurn0O5Zv7okJniY8\nubr97/MKT8niGa4jrM6fH7KxlMNTD4/cpmCqWDGSwQKBgQD6nF34/sUowraqACpq\na0K1xVugX/ShzOBejz82XOndKugJUilNIZ3ADLRt7PD0hbZrnaJiJ2IWCLy7E2SF\nHk0q3gN1FciQ4ZwozddkX/a8FZ6bNQ2rfPIqeTOhqgmeT5g+iuqH1cFeJvp75wYG\ngGJb4tgsAKPSP/FdEz3GUxoTQQKBgQDRZzLrMIHTQ4QbFobieW51ehzUHkgSZVdW\nJxIqNAWJehsCWYSIA64Tch/Z3VApPDtlPW8iEO7NJPQ8bn87paJ5UMruXM709Yoc\nuUNZ5voaSy9CyqSr5AJkMXAmIzKVhPiEJhxSoWyc6iOCUltxYCvGhXOdlZFB4Jg6\ncr2KIdcL+wKBgQCWRCwUoK6XQilExdSo+qeFVlEaWG7WlYHzMPE2J4ASnSeEbz9U\ngFOWVAfb4baY55P58wrqz7KAg7ymb3pWV1Tt3zEzg1w/gwefb8024lOPc2cmqSkJ\nKmIKFaJfzRT3V8JxvdnimMjZK4YFxQBX5aMUdXAo8jlIWA7hQChKQongAQKBgDHM\n2z03wgheTDKs/tMpke16ZxTDRtypIUYp8JX83Tw+v+GlLHG0fle9knmbuo6z9mQz\nYhS19jTvju44USX8VFGJt1lM1SNIqdn+7VeaKOzL6Kd3zgXbKf5uC3/mawdIIlo7\nPg0gutXukRdTQtAJ0R9baY2Wnn8OTOXJl+4b8iq1AoGBANAaCBMgLoNP8pLr0tEt\nHQYqIAF2lMX2JiGyC5DZgbxWjsiU5YryHZ0BeOy0gf+6M4hgEENhmbnO4bo1ng5T\ndwDOMKG2G3eY3brGPih1PWqcic9i8Zb1esguCcOt1S19IxULEx48xNorn5L7HqAs\nUxDnfuBnO9M3IQmMQqre6ZYW\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-cl9gh@fir-testing-bd63e.iam.gserviceaccount.com",
+  "client_id": "100781005516283060231",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-cl9gh%40fir-testing-bd63e.iam.gserviceaccount.com"
 }
