975308163e7a31d07a5b5f1d887e4f7fe97695f7
jestes0 <39225989+jestes0@users.noreply.github.com>
2020-02-09 08:58:48 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,118 +1,31 @@
-// www/pages/login.js
+import {getCode} from '../utils/helperFunctions';
+import ImageHeader from '../components/ImageHeader';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
-import { Component } from 'react'
-import fetch from 'isomorphic-unfetch'
-import Layout from '../components/layout'
-import { login } from '../utils/auth'
-
-class Login extends Component {
-  static getInitialProps ({ req }) {
-    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
-
-    const apiUrl = process.browser
-      ? `${protocol}://${window.location.host}/api/login.js`
-      : `${protocol}://${req.headers.host}/api/login.js`
-
-    return { apiUrl }
-  }
-
-  constructor (props) {
-    super(props)
-
-    this.state = { username: '', error: '' }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleChange (event) {
-    this.setState({ username: event.target.value })
-  }
-
-  async handleSubmit (event) {
-    event.preventDefault()
-    const username = this.state.username
-    const url = this.props.apiUrl
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username })
-      })
-      if (response.ok) {
-        const { token } = await response.json()
-        login({ token })
-      } else {
-        console.log('Login failed.')
-        // https://github.com/developit/unfetch#caveats
-        let error = new Error(response.statusText)
-        error.response = response
-        return Promise.reject(error)
-      }
-    } catch (error) {
-      console.error(
-        'You have an error in your code or there are Network issues.',
-        error
-      )
-      throw new Error(error)
-    }
-  }
-
-  render () {
+const LogInForm = () => {
     return (
-      <Layout>
-        <div className='login'>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor='username'>GitHub username</label>
+        <html>
+        <head>
+        <script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script>
+        <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" />
+        </head>
+        </html>
+    );
+};
 
-            <input
-              type='text'
-              id='username'
-              name='username'
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-
-            <button type='submit'>Login</button>
-
-            <p className={`error ${this.state.error && 'show'}`}>
-              {this.state.error && `Error: ${this.state.error}`}
-            </p>
-          </form>
+let Login = () =>
+    <div className="login-body">
+        <ImageHeader />
+        <div className="login-form">
+        <LogInForm />
         </div>
-        <style jsx>{`
-          .login {
-            max-width: 340px;
-            margin: 0 auto;
-            padding: 1rem;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-          }
-          form {
-            display: flex;
-            flex-flow: column;
-          }
-          label {
-            font-weight: 600;
-          }
-          input {
-            padding: 8px;
-            margin: 0.3rem 0 1rem;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-          }
-          .error {
-            margin: 0.5rem 0 0;
-            display: none;
-            color: brown;
-          }
-          .error.show {
-            display: block;
-          }
-        `}</style>
-      </Layout>
-    )
-  }
-}
+        <div className="login-register">
+            <p>Not registered yet{getCode(63)}</p>
+            {/** need on click for the buttons below */}
+            <button type="button" name="customerBtn">Register</button>
+            <button type="button" name="adminBtn">Administrator Registration</button>
+        </div>
+    </div>;
 
-export default Login
+export default Login;
