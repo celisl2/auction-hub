96bcbdf9ae54c770369ce86c13a9a0926cf2c329
cashtonCode <44584924+cashtonCode@users.noreply.github.com>
2020-02-11 12:24:10 -0500

    Update authenticate.js
    Changing the values to authorize the auction-hub instead of the test-hub

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -9,17 +9,16 @@
 function authenticate () {
     // Import necessary modules
     var admin = require("firebase-admin");
-    var crypto = require('crypto');
 
     // Authenticate with firebase via service account.
     // I'm not sure about node applications running on Firebase itself.
     // Replace the below with the service credentials provided on Firebase
-    var serviceAccount = require("./Credentials/fir-testing-bd63e-firebase-adminsdk-cl9gh-1d1e237b01.json");
+    var serviceAccount = require("path/to/serviceAccountKey.json");
 
     // Initialize the authentication.
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: "https://fir-testing-bd63e.firebaseio.com"
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://auction-hub-firebase.firebaseio.com"
     });
 
     // Shorthand access to database
@@ -28,3 +27,14 @@ function authenticate () {
     // The following stops some console log errors and warnings.
     db.settings({timestampInSnapshots: true});
 }
+//TEST FIREBASE - DELETE LATER
+// var admin = require("firebase-admin");
+//
+//
+// var serviceAccount = require("./Credentials/fir-testing-bd63e-firebase-adminsdk-cl9gh-1d1e237b01.json");
+//
+//
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://fir-testing-bd63e.firebaseio.com"
+// });
