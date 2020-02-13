a03c49ab17157bba0c4ae22757a54b2f474b5b35
cashtonCode <44584924+cashtonCode@users.noreply.github.com>
2020-02-13 10:06:07 -0500

    Update get_bid_amount.js
    @Laura here's what I've been working on

--- pages/api/get_bid_amount.js
+++ pages/api/get_bid_amount.js
@@ -29,7 +29,59 @@ function get_bid_amount (bidHistoryId) {
     db.settings({timestampInSnapshots: true});
 
 
+
+    var bidPath = '/AuctionEvent/vOwsw3o6VFijgeOEDKXM/AuctionProduct/lF49ndHaQI3CCwJ6E0tI/BidHistory/6yOVgNynlTu3aK6r7QhK';
+
     // query the database using bidHistoryId and return just the amount number value
-    // amount = //query//
+    // READ DATA ONCE
+    // Firebase App (the core Firebase SDK) is always required and
+    // must be listed before other Firebase SDKs
+    var firebase = require("firebase/app");
+
+    // Add the Firebase products that you want to use
+    require("firebase/auth");
+    require("firebase/firestore");
+
+    // work that needs to be done:
+    //  narrow field to one field - amount
+    //  save amount value to a variable
+    //  return value
+
+    let promise = new Promise((resolve, reject) => {
+
+    })
+
+    db.collection(bidHistoryId).get()
+        .then((snapshot) => {
+            snapshot.forEach(doc => {
+                // console.log(doc.id, '=>', doc.data().amount); // print statement // remove once done
+                let amount = await doc.data().amount;
+            });
+        })
+        .catch((err) => {
+            console.log('Error getting documents', err); // print statement // remove once done
+        });
+
+
+    return amount;
+
+    // const promise = admin.firestore().doc(bidPath).get();
+    // promise.then(snapshot => {
+    //     const data = snapshot.data();
+    //     response.send(data);
+    // })
+    // .catch(error => {
+    //     //handle error
+    //     console.log(error);
+    //     response.status(500).send(error);
+    // });
+
+
+    // amount =
     // return amount;
 }
+
+//testing function output
+// console.log("\t" + get_bid_amount("6yOVgNynlTu3aK6r7QhK"))
+path = '/AuctionEvent/vOwsw3o6VFijgeOEDKXM/AuctionProduct/lF49ndHaQI3CCwJ6E0tI/BidHistory'
+console.log(get_bid_amount(path));
