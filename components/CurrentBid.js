258b6db656452a7ad168fe1682ee885ca8d2994e
LauraC <celisl2@mailbox.winthrop.edu>
2020-03-30 14:38:10 -0400

    current highest bid displaying correctly

--- components/CurrentBid.js
+++ components/CurrentBid.js
@@ -1,22 +1,64 @@
 import {useState, useContext, useEffect} from 'react';
 import HighestBidContext from '../lib/highestBidContext';
-import readHighestBid from '../pages/api/readHighestBid';
-//let x = readHighestBid(props.data.auctionID, props.data.productID, 0);
+import {loadDB} from '../lib/db';
+let db = loadDB();
 
-// props -> minBid
+/**
+ * @param {data: {mindBid, productID, auctionID}} props 
+ */
 const CurrentBid = (props) => {
-    const [bid, setBid] = useState();
-    const [highestBid, setHighestBid] = useState(0);
+    const [highBidData, setHighBidData] = useState(() => {
+        db
+        .firestore()
+        .collection('/AuctionEvent/' + aucID + '/AuctionProduct/' + prodID + '/BidHistory')
+        .orderBy('amount', 'desc')
+        .limit(1)
+        .onSnapshot( (snapshot) => {
+            if (snapshot.size) {
+                //console.log("DEBUG: Active Event Snapshot Size: " + snapshot.size);
+                let highBidDataInitial = snapshot.docs[0].data().amount;
+                return highBidDataInitial;
+            }
+            
+            else {
+                //console.log("No currrent high bid accquired. The DB may be no bids for this product.\n" + "AuctionEventID: " + aucID + " ,    ProductID: " + prodID);
+                return null;
+            }
+        });
+    });
 
-   
+    let aucID = props.data.auctionID;
+    let prodID = props.data.productID;
+    let minBid = props.data.minBid;
+    
+    useEffect(() => {
+        const unsubscribe = db
+        .firestore()
+        .collection('/AuctionEvent/' + aucID + '/AuctionProduct/' + prodID + '/BidHistory')
+        .orderBy('amount', 'desc')
+        .limit(1)
+        .onSnapshot( (snapshot) => {
+            if (snapshot.size) {
+                let highBid = snapshot.docs[0].data().amount;
+                setHighBidData(highBid);
+            }
+        });
+
+        return () => { unsubscribe() };
+    }, [db]);
+
     return (
         <div>
-            <p className="lightBlue">{bid ? "Current Bid" : "Starting Bid"}</p>
-           
-            {readHighestBid(props.data.auctionID, props.data.productID) ? <p className="padding">{'$' + readHighestBid(props.data.auctionID, props.data.productID).amount}</p>  : <p className="padding">{'$' + props.data.minBid}</p>}
-            
+            {highBidData ? 
+                <>
+                    <p className="lightBlue">Current Bid</p>
+                    <p className="padding">{ "$" + highBidData}</p>
+                </> :
+                <>
+                    <p className="lightBlue">Minimum Price for Starting Bid</p>
+                    <p className="padding">{"$" + minBid}</p>
+                </> }
         </div>
     )
 };
 export default CurrentBid;
-    
